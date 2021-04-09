ed3c93a81c58ef4e9bffe0aefbbe8510a15df78a
EnriOv <orrantia.en@gmail.com>
2021-04-09 01:08:36 -0600

    Added design of website.

---
+++ react-application/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import Song from './components/Song'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Song />
+    </div>
+  );
+}
+
+export default App;
