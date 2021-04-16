91eb95e75b60a8dbdb88654ac8d47b69c6df4d62
EnriOv <orrantia.en@gmail.com>
2021-04-16 14:28:10 -0600

    Added code to generate the songs using the neural network model.

--- react-application/src/components/Generator.js
+++ react-application/src/components/Generator.js
@@ -1,8 +1,85 @@
-import { useState } from 'react' 
+import { useState, useEffect } from 'react' 
 import SongInfo from "./SongInfo"
+import * as tf from '@tensorflow/tfjs';
 
 const Generator = () => {
     const [song, setSong] = useState('');
+    const [model, setModel] = useState(null);
+    const [char2idx, setChar2idx] = useState({})
+
+    const vocab = ['\n', ' ', '!', '"', '#', '\'', '(', ')', ',', '-', '.', '/', '0', '1',
+    '2', '3', '4', '5', '6', '7', '8', '9', ':', '<', '=', '>', 'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z', '[', ']', '^', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '|'];
+
+    const url = {
+        model: 'https://irish-song-rnn-model-bucket.s3-us-west-1.amazonaws.com/irish_rnn/model.json',
+    };
+
+    async function loadModel(url) {
+        try {
+            const model = await tf.loadLayersModel(url.model);
+
+            setModel(model);
+            console.log("Load model success");
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    const onGenerateSong = () => {
+        const length = 500;
+        const start_string = "X:1\nT:";
+
+        if(model) {
+            var gen_text = [];
+
+            /* Convert the start string to numbers (vectorize) */
+            var input_eval = start_string.split('').map(letter => {return char2idx[letter]});
+
+            input_eval = tf.expandDims(input_eval, 0);
+            
+            for(var counter = 0; counter < length; counter++) {
+                /* Evaluate the inputs and generate the next character predictions */
+                var predictions = model.predict(input_eval);
+
+                /* Remove the batch dimension */
+                predictions = tf.squeeze(predictions, 0);
+
+                /* Use a multinomial distribution to sample */
+                var predicted_id = tf.multinomial(predictions, 1)
+
+                const values = predicted_id.dataSync();
+                const arr = Array.from(values);
+                predicted_id = arr[0];
+                
+                /* Pass the prediction along with the previous hidden state 
+                   as the next inputs to the model */
+                input_eval = tf.expandDims([predicted_id], 0);
+
+                /* Add the predicted character to the generated text! */
+                gen_text.push(vocab[predicted_id]);
+            }
+
+            setSong(start_string + gen_text.join(''));
+        }
+    }
+
+    useEffect(()=>{
+        const objectMapping = {};
+
+        tf.ready().then(()=>{
+            loadModel(url);
+        });
+
+        for(const [index, element] of vocab.entries()) {
+            objectMapping[element] = index;
+        }
+
+        setChar2idx(objectMapping);
+    }, [])
 
     return (
         <>
@@ -18,16 +95,11 @@ const Generator = () => {
                     MIT 6.S191: Introduction to Deep Learning<br/>
                     IntroToDeepLearning.com
                 </p>
-                <br/>
-                <p>
-                    Source to code:<br/>
-                    Link to code
-                </p>
                 <div className='generate-btn-container'>
-                    <button className='btn generate-btn'>GENERATE SONG</button>
+                    <button className='btn generate-btn' onClick={onGenerateSong}>GENERATE SONG</button>
                 </div>
             </div>
-            <SongInfo />
+            <SongInfo song={song}/>
         </>
     )
 }
--- react-application/src/components/SongInfo.js
+++ react-application/src/components/SongInfo.js
@@ -3,8 +3,12 @@ import 'font-awesome/css/font-awesome.min.css';
 import 'abcjs/abcjs-midi.css';
 import abcjs from 'abcjs/midi';
 
-const SongInfo = () => {
-    const abc = "T:Huwckartye\n"
+const SongInfo = (props) => {
+    const { song } = props;
+
+    const [notes, setNotes] = useState('');
+
+    var abc = "T:Huwckartye\n"
             + "Z: id:dc-jig-122\n"
             + "M:6/8\n"
             + "L:1/8\n"
@@ -14,20 +18,21 @@ const SongInfo = () => {
             + "g2fg ebab|gdfA d2ed|cAeB AFEG|Aed^c d3:|!\n"
             + "BdB|eAA2 BAGA|B2dB GBdB|cABG AGEG|BAGF D2:|!\n";
 
-    const newLineBreak = text => {
-        return text.split('\n').map(function (line) {
-            return (<p>{line}</p>);
-        })
-    }
-
     useEffect(() => {
-        // Draw the music - this supplies an object that has a lot of information about how to create the synth.
-        var visualObj = abcjs.renderAbc('paper', abc, {responsive: 'resize' })[0];
+        if(song) {
+            abc = "X:" + song.split('X:')[1];
+        }
+
+        /* Draw the music - this supplies an object that has a lot of information about 
+           how to create the synth. */
+        const visualObj = abcjs.renderAbc('paper', abc, {responsive: 'resize' })[0];
         
         abcjs.renderMidi('media-player', abc, { });
 
         abcjs.midi.setLoop(document.querySelector('.abcjs-inline-midi'), true);
-    }, [])
+
+        setNotes(abc)
+    }, [song])
 
     return (
         <>
@@ -35,7 +40,7 @@ const SongInfo = () => {
                 <div id='media-player'>Media Player</div>
             </div>
             <div className='song-abc'>
-                {newLineBreak(abc)}
+                {notes}
             </div>
             <div>
                 <div id='paper'></div>
